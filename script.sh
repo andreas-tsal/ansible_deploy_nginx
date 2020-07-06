91dffb7f617c0febe9b935670f509e9b5cd0182e
tsalidis <tsalidis96@gmail.com>
2020-07-06 09:57:09 +0300

    not default ansible.cfg

--- script.sh
+++ script.sh
@@ -6,10 +6,9 @@ sudo apt-get upgrade -y
 sudo apt install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update -y
-sudo apt install ansible -y
+sudo apt install ansible --yes
 
-sudo unlink ansible.cfg
-sudo unlink hosts
+sudo unlink /etc/ansible/ansible.cfg.dpkg-new
 sudo rm -rf roles/
 
 sudo sleep 2
